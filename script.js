44e5e42178c5a2b5c05247799e5773872eba3d3e
yashrajbharticybtekk <y.bharti@cybtekk.com>
2024-09-27 11:57:41 +0530

    add: support for custom color

--- script.js
+++ script.js
@@ -204,17 +204,23 @@ toolBtns.forEach((btn) => {
   });
 });
 
-colorBtns.forEach((color) => {
-  color.addEventListener("click", () => {
-    colorBtns.forEach((c) => c.classList.remove("selected"));
-    color.classList.add("selected");
-    selectedColor = window.getComputedStyle(color).backgroundColor;
+colorBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    // adding click event to all color button
+    // removing selected class from the previous option and adding on current clicked option
+    document.querySelector(".options .selected").classList.remove("selected");
+    btn.classList.add("selected");
+    // passing selected btn background color as selectedColor value
+    selectedColor = window
+      .getComputedStyle(btn)
+      .getPropertyValue("background-color");
   });
 });
 
-colorPicker.addEventListener("input", (e) => {
-  selectedColor = e.target.value;
-  colorBtns.forEach((color) => (color.style.backgroundColor = selectedColor));
+colorPicker.addEventListener("change", () => {
+  // passing picked color value from color picker to last color btn background
+  colorPicker.parentElement.style.background = colorPicker.value;
+  colorPicker.parentElement.click();
 });
 
 sizeSlider.addEventListener("input", (e) => {
